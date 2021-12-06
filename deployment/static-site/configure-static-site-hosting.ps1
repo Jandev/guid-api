06923c37fb8fbe67c56454026e55a438befd6648
Jan de Vries <jan@jan-v.nl>
2021-12-06 20:36:54 +0100

    Adjusting the workflow with static site

--- deployment/static-site/configure-static-site-hosting.ps1
+++
@@ -1,2 +0,0 @@
-$storageAccount = "guidapistaticprodweu"
-az storage blob service-properties update --account-name $storageAccount --static-website --index-document index.html --404-document 404.html
