80c81034aa3a90cf6651a07a97814e4909f9a454
Ronald van der Plas <r.vdplas@quicknet.nl>
2022-01-13 09:13:06 +0100

    Added JSON support. (#12)
    * Added JSON support.
    
    * Refactored strings to MediaTypeNames

--- src/Api/NewGuid.cs
+++ src/Api/NewGuid.cs
@@ -2,21 +2,46 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Api
 {
     public static class NewGuid
     {
         [FunctionName("DefaultNewGuid")]
-        public static string Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = null)] 
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = null)]
             HttpRequest req,
             ILogger log)
         {
             var guid = Guid.NewGuid().ToString("D");
             log.LogInformation("Created guid `{0}`", guid);
-            return guid;
+
+            var mimeTypeRequest = req.Headers["Accept"].FirstOrDefault();
+
+            return CreateContentByAcceptHeader(mimeTypeRequest, guid);
+        }
+
+        private static HttpResponseMessage CreateContentByAcceptHeader(string mimeTypeRequest, string guid)
+        {
+            return mimeTypeRequest switch
+            {
+                MediaTypeNames.Application.Json => new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(new { value = guid }), Encoding.UTF8,
+                        MediaTypeNames.Application.Json)
+                },
+                _ => new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(guid, Encoding.UTF8, MediaTypeNames.Text.Plain)
+                }
+            };
         }
     }
 }
